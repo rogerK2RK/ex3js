b9548fcd9237b25328740b28881bf088dafebe00
Roger Retita <roger.retita@gmail.com>
2024-10-16 14:07:16 +0200

    add event click and get departement for each region

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
-const baseUrlDepart = 'https://geo.api.gouv.fr';
+const baseUrl = 'https://geo.api.gouv.fr';
+const regtList = document.querySelector('#region-list');
 const departList = document.querySelector('#depart-list');
 
 // Liste de tous les départements de France
-fetch(`${baseUrlDepart}/departements`)
-    .then(response => response.json())  // Correction ici, 'response' au lieu de 'reponse'
+fetch(`${baseUrl}/regions`)
+    .then(response => response.json())
     .then(data => {
-        departList.innerHTML = data.map(departement => {
-            console.log(departement)
-            return `<option value="${departement.nom}">${departement.nom}</option>`;
+        regtList.innerHTML = data.map(region => {
+            // console.log(region);
+            return `<option value="${region.code}">${region.nom}</option>`;
         }).join('');
     })
-    .catch(error => console.error('Erreur:', error));  // Ajout d'une gestion d'erreur
+    // Ajout d'une gestion d'erreur
+    .catch(error => console.error('Erreur:', error));  
+
+
+// Récuperer la valeur du département au click 
+regtList.addEventListener('change', (e) => {
+    const selectedReg = e.target.value;
+    console.log(selectedReg);
+    callDepart(selectedReg);
+
+})
+
+async function callDepart(code) {
+    fetch(`${baseUrl}/regions/${code}/departements`)
+    .then(response => response.json())
+    .then(data => {
+        departList.innerHTML = data.map(departement => {
+            // console.log(region);
+            return `<option value="${departement.code}">${departement.nom}</option>`;
+        }).join('');
+    })
+    // Ajout d'une gestion d'erreur
+    .catch(error => console.error('Erreur:', error)); 
+}
